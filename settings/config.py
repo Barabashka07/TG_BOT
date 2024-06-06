05d54892fb90d48eb138ee16bd17e042d76026b7
Артём Кечкин <115799751+Barabashka07@users.noreply.github.com>
2024-06-06 12:35:24 +0300

    TGBOT

---
+++ settings/config.py
@@ -0,0 +1,44 @@
+import os
+from emoji import emojize
+
+TOKEN = '6444163261:AAHL8AvFdoUXLeylFfXh34tmrUX3gRjajIg'
+NAME_DB = 'products.db'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = os.path.join('sqlite:///'+BASE_DIR, NAME_DB)
+
+COUNT = 0
+
+# кнопки управления
+KEYBOARD = {
+    'CHOOSE_GOODS': emojize('🛒 Выбрать товар'),
+    'INFO': emojize(':speech_balloon: О магазине'),
+    'SETTINGS': emojize('⚙️ Настройки'),
+    'PHONES': emojize('📱 Телефоны'),
+    'LAPTOP': emojize('💻 Ноутбуки'),
+    'HEADPHONES': emojize('🎧 Наушники'),
+    '<<': emojize('⏪'),
+    '>>': emojize('⏩'),
+    'BACK_STEP': emojize('◀️'),
+    'NEXT_STEP': emojize('▶️'),
+    'ORDER': emojize('🛒 Заказ'),
+    'X': emojize('❌'),
+    'DOUWN': emojize('🔽'),
+    'AMOUNT_PRODUCT': COUNT,
+    'AMOUNT_ORDERS': COUNT,
+    'UP': emojize('🔼'),
+    'APPLAY': '✅ Оформить заказ',
+}
+
+# id категорий продуктов
+CATEGORY = {
+    'PHONES': 1,
+    'LAPTOP': 2,
+    'HEADPHONES': 3,
+}
+
+# названия команд
+COMMANDS = {
+    'START': "start",
+    'HELP': "help",
+}
